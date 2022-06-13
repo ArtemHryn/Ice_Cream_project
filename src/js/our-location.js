2b6692cee0442c5056cd0591b0b46b3be930291a
ArtemHryn <grynbest@gmail.com>
2022-06-13 03:12:58 +0300

    our location modal window

---
+++ src/js/our-location.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.js-our-location-open'),
+    closeModalBtn: document.querySelector('.js-our-location-close'),
+    modal: document.querySelector('.js-our-location-window'),
+    body: document.querySelector('body'),
+    
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
