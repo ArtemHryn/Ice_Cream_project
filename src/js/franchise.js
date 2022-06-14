0ef81830a3afd92e71766f3844a343d2990b9472
OlyaNegrych <olyanegrych@gmail.com>
2022-06-14 22:59:58 +0300

    fixed franchise modal window display

--- src/js/franchise.js
+++ src/js/franchise.js
@@ -1,17 +1,19 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('.js-franchise-open'),
-    closeModalBtn: document.querySelector('.js-franchise-close'),
-    modal: document.querySelector('.js-franchise-window'),
+    openFranchiseBtn: document.querySelector('.js-franchise-open'),
+    closeFranchiseBtn: document.querySelector('.js-franchise-close'),
+    modalFranchiseWindow: document.querySelector('.js-franchise-window'),
     body: document.querySelector('body'),
-    
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openFranchiseBtn.addEventListener('click', toggleModalFranchiseWindow);
+  refs.closeFranchiseBtn.addEventListener('click', toggleModalFranchiseWindow);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+  console.log(refs.openFranchiseBtn);
+  console.log(refs.closeFranchiseBtn);
+
+  function toggleModalFranchiseWindow() {
+    refs.modalFranchiseWindow.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll-not-for-Yura');
   }
 })();
